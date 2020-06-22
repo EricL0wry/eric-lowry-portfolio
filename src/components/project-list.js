b28fa52f2d1ccf3b7c1cc5835682fee5d67196ad
Eric Lowry <ericmichaellowry@gmail.com>
2020-06-22 16:39:02 -0700

    Created project-list.js document to hold an array of project data

---
+++ src/components/project-list.js
@@ -0,0 +1,31 @@
+const projectList = [
+  {
+    name: 'Into the Groove',
+    shortDesc: 'Record Store',
+    desc: 'A mobile-responsive full-stack eCommerce vinyl record store application utilizing the PERN stack',
+    github: 'https://github.com/EricL0wry/react-record-store',
+    live: 'https://record-store.ericmichaellowry.com',
+    image: './images/record-store-card.png',
+    stack: ['JavaScript', 'React.js', 'Node.js', 'Express', 'PostgreSQL', 'Bootstrap', 'HTML5', 'CSS3']
+  },
+  {
+    name: 'BasedSales',
+    shortDesc: 'CRM Platform',
+    desc: 'A mobile-first full-stack Customer Relationship Management (CRM) platform utilizing the PERN stack',
+    github: 'https://github.com/EricL0wry/crm-platform',
+    live: 'https://crm.ericmichaellowry.com',
+    image: './images/crm-card.png',
+    stack: ['JavaScript', 'React.js', 'Node.js', 'Express', 'PostgreSQL', 'Material-UI', 'React Router', 'API', 'HTML5', 'CSS3']
+  },
+  {
+    name: 'Where ISS It?',
+    shortDesc: 'ISS Tracker',
+    desc: 'A mobile-responsive International Space Station (ISS) tracking application utilizing OOP and Google APIs',
+    github: 'https://github.com/EricL0wry/iss-tracker',
+    live: 'https://iss-tracker.ericmichaellowry.com',
+    image: './images/iss-tracker-card.png',
+    stack: ['JavaScript', 'Node.js', 'Express', 'jQuery AJAX', 'Bootstrap', 'OOP', 'Google APIs', 'HTML5', 'CSS3']
+  }
+];
+
+export default projectList;
