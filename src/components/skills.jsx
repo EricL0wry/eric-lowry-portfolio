2019f2375350956fea2ef6cf1c944200ca88ec13
Eric Lowry <ericmichaellowry@gmail.com>
2020-06-23 12:57:52 -0700

    Created containers for skills component and title row

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -3,6 +3,13 @@ import React from 'react';
 export default function Skills(props) {
 
   return (
-    <h1>Skills</h1>
+    <section className="skils">
+      <div className="skills-content container sp-top sp-bottom">
+        <div className="skills-title row flex-column justify-content-center align-items-center">
+          <h4>Skills</h4>
+          <h1>What I can do</h1>
+        </div>
+      </div>
+    </section>
   );
 }
