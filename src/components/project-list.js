d410d936c1de4861e16c1b36504e5f5066462e33
Eric Lowry <ericmichaellowry@gmail.com>
2020-06-22 17:22:08 -0700

    Added alt property to each project object

--- src/components/project-list.js
+++ src/components/project-list.js
@@ -6,6 +6,7 @@ const projectList = [
     github: 'https://github.com/EricL0wry/react-record-store',
     live: 'https://record-store.ericmichaellowry.com',
     image: './images/record-store-card.png',
+    alt: 'Record Store Screen Shot',
     stack: ['JavaScript', 'React.js', 'Node.js', 'Express', 'PostgreSQL', 'Bootstrap', 'HTML5', 'CSS3']
   },
   {
@@ -15,6 +16,7 @@ const projectList = [
     github: 'https://github.com/EricL0wry/crm-platform',
     live: 'https://crm.ericmichaellowry.com',
     image: './images/crm-card.png',
+    alt: 'CRM Platform Logo',
     stack: ['JavaScript', 'React.js', 'Node.js', 'Express', 'PostgreSQL', 'Material-UI', 'React Router', 'API', 'HTML5', 'CSS3']
   },
   {
@@ -24,6 +26,7 @@ const projectList = [
     github: 'https://github.com/EricL0wry/iss-tracker',
     live: 'https://iss-tracker.ericmichaellowry.com',
     image: './images/iss-tracker-card.png',
+    alt: 'ISS Tracker Screen Shot',
     stack: ['JavaScript', 'Node.js', 'Express', 'jQuery AJAX', 'Bootstrap', 'OOP', 'Google APIs', 'HTML5', 'CSS3']
   }
 ];
