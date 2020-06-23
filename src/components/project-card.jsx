37334df8cd18138570b62f9bc92a345860eb18d5
Eric Lowry <ericmichaellowry@gmail.com>
2020-06-22 17:58:29 -0700

    Created basic card to render image, name, and short description

--- src/components/project-card.jsx
+++ src/components/project-card.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 
 export default function ProjectCard(props) {
-  const { image, alt } = props;
+  const { image, alt, name, shortDesc } = props.project;
 
   return (
     <div className='col-lg-4'>
       <div className="card project-card">
         <img src={image} alt={alt} className="card-img-top"/>
         <div className="card-body">
-          <h1>Card Body</h1>
+          <h2 className="card-title">{name}</h2>
+          <h4 className="card-subtitle">{shortDesc}</h4>
         </div>
       </div>
     </div>
