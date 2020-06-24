ffb9ffe4e650e211abf91742688a592d7e6c4e45
Eric Lowry <ericmichaellowry@gmail.com>
2020-06-24 12:35:03 -0700

    Updated skill component to render a card using props passed from skills parent component

--- src/components/skill.jsx
+++ src/components/skill.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 
 export default function Skill(props) {
-
+  const { name, img } = props;
   return (
-    <h1>Skill</h1>
+    <div className="col-lg-4 skill-card d-flex flex-column justify-content-center align-items-center">
+      <div className="skill-img-container d-flex justify-content-center">
+        <img className="skill-img" src={img} alt={name} />
+      </div>
+      <div className="skill-name-container">
+        <h4>{name}</h4>
+      </div>
+    </div>
   );
 }
