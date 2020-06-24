0f82eb116eddd33cab0f74d44f6c5795c2c5f8af
Eric Lowry <ericmichaellowry@gmail.com>
2020-06-23 18:13:04 -0700

    Added an object for each skill with image url, name, and id number

--- src/components/skills-list.js
+++ src/components/skills-list.js
@@ -1,5 +1,24 @@
 const skillsList = [
-
+  {
+    id: 1,
+    name: 'JavaScript',
+    img: './images/skills/javascript-original.svg'
+  },
+  {
+    id: 2,
+    name: 'React.js',
+    img: './images/skills/react-original.svg'
+  },
+  {
+    id: 3,
+    name: 'Node.js',
+    img: './images/skills/nodejs-original.svg'
+  },
+  {
+    id: 4,
+    name: 'HTML5',
+    img: './images/skills/html5-original.svg'
+  }
 ];
 
 export default skillsList;
