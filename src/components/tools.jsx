58a0528d03073102f06284909370c87fbf451770
Eric Lowry <ericmichaellowry@gmail.com>
2020-06-24 19:36:41 -0700

    Updated title verbiage slightly

--- src/components/tools.jsx
+++ src/components/tools.jsx
@@ -12,7 +12,7 @@ export default function Tools(props) {
       <div className="tools-content container sp-top sp-bottom">
         <div className="tools-title row flex-column justify-content-center align-items-center">
           <h4>Tools</h4>
-          <h1>What I use to do it</h1>
+          <h1>How I do it</h1>
         </div>
         <div className="tools-cards row">
           {tools}
