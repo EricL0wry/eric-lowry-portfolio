616338fa5ab4fefab41953797110484b24da9775
Eric Lowry <ericmichaellowry@gmail.com>
2020-06-24 19:24:12 -0700

    Imported tool component and mapped through props to create and render array of tool cards

--- src/components/tools.jsx
+++ src/components/tools.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import Tool from './tool';
 
 export default function Tools(props) {
+  const tools = props.tools.map(toolObj => {
+    const { id, name, img } = toolObj;
+    return <Tool key={id} name={name} img={img} />;
+  });
 
   return (
-    <h1>Tools</h1>
+    <section className="tools">
+      <div className="tools-content container sp-top sp-bottom">
+        <div className="tools-title row flex-column justify-content-center align-items-center">
+          <h4>Tools</h4>
+          <h1>What I use to do it</h1>
+        </div>
+        <div className="tools-cards row">
+          {tools}
+        </div>
+      </div>
+    </section>
   );
 }
