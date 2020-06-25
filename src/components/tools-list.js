b83d7bd7e98ae160046702d70bd87325e0e087e4
Eric Lowry <ericmichaellowry@gmail.com>
2020-06-24 19:14:08 -0700

    Populated img property with image file name for each tool object

--- src/components/tools-list.js
+++ src/components/tools-list.js
@@ -2,47 +2,47 @@ const toolsList = [
   {
     id: 1,
     name: 'AWS',
-    img: './images/tools/*.svg'
+    img: './images/tools/amazonwebservices-original.svg'
   },
   {
     id: 2,
     name: 'Serverless',
-    img: './images/tools/*.svg'
+    img: './images/tools/serverless.svg'
   },
   {
     id: 3,
     name: 'Git/GitHub',
-    img: './images/tools/*.svg'
+    img: './images/tools/github-original.svg'
   },
   {
     id: 4,
     name: 'Webpack',
-    img: './images/tools/*.svg'
+    img: './images/tools/webpack-original.svg'
   },
   {
     id: 5,
     name: 'npm',
-    img: './images/tools/*.svg'
+    img: './images/tools/npm-original-wordmark.svg'
   },
   {
     id: 6,
     name: 'Babel',
-    img: './images/tools/*.svg'
+    img: './images/tools/babel-original.svg'
   },
   {
     id: 7,
     name: 'VSCode',
-    img: './images/tools/*.svg'
+    img: './images/tools/visualstudio-plain.svg'
   },
   {
     id: 8,
     name: 'Nginx',
-    img: './images/tools/*.svg'
+    img: './images/tools/nginx-original.svg'
   },
   {
     id: 9,
     name: 'Illustrator',
-    img: './images/tools/*.svg'
+    img: './images/tools/illustrator-plain.svg'
   }
 ];
 
