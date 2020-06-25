126455361dd60764047a35cba2645a89d078f36c
Eric Lowry <ericmichaellowry@gmail.com>
2020-06-24 19:04:25 -0700

    Created base list of tools in tools-list.js

---
+++ src/components/tools-list.js
@@ -0,0 +1,49 @@
+const toolsList = [
+  {
+    id: 1,
+    name: 'AWS',
+    img: './images/tools/*.svg'
+  },
+  {
+    id: 2,
+    name: 'Serverless',
+    img: './images/tools/*.svg'
+  },
+  {
+    id: 3,
+    name: 'Git/GitHub',
+    img: './images/tools/*.svg'
+  },
+  {
+    id: 4,
+    name: 'Webpack',
+    img: './images/tools/*.svg'
+  },
+  {
+    id: 5,
+    name: 'npm',
+    img: './images/tools/*.svg'
+  },
+  {
+    id: 6,
+    name: 'Babel',
+    img: './images/tools/*.svg'
+  },
+  {
+    id: 7,
+    name: 'VSCode',
+    img: './images/tools/*.svg'
+  },
+  {
+    id: 8,
+    name: 'Nginx',
+    img: './images/tools/*.svg'
+  },
+  {
+    id: 9,
+    name: 'Illustrator',
+    img: './images/tools/*.svg'
+  }
+];
+
+export default toolsList;
