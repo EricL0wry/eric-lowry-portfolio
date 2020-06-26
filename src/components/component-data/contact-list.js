be473c6cf333c0bd6934c9e44a4cb3f2885658f4
Eric Lowry <ericmichaellowry@gmail.com>
2020-06-25 18:04:26 -0700

    Completed conatact list file containing an array of contacts to be rendered dynamically

---
+++ src/components/component-data/contact-list.js
@@ -0,0 +1,47 @@
+const contactList = [
+  {
+    id: 1,
+    type: 'single',
+    label: 'Location',
+    text: 'Orange County, CA',
+    url: 'https://www.google.com/maps/search/?api=1&Orange+County%2C+CA'
+  },
+  {
+    id: 2,
+    type: 'single',
+    label: 'Email',
+    text: 'ericmichaellowry@gmail.com',
+    url: 'mailto:ericmichaellowry@gmail.com'
+  },
+  {
+    id: 3,
+    type: 'single',
+    label: 'Phone',
+    text: '+1 (714) 482-3670',
+    url: 'tel:+1-714-482-3670'
+  },
+  {
+    id: 4,
+    type: 'multiple',
+    label: 'Social & Resume',
+    text: [
+      {
+        id: 1,
+        icon: 'fab fa-linkedin',
+        url: 'https://www.linkedin.com/in/ericmichaellowry/'
+      },
+      {
+        id: 2,
+        icon: 'fab fa-github',
+        url: 'https://github.com/EricL0wry'
+      },
+      {
+        id: 3,
+        icon: 'fas fa-file-alt',
+        url: 'https://drive.google.com/file/d/1IW3Nkgi9qp-nx-wFg7EGFdcMwQqmNska/view'
+      }
+    ]
+  }
+];
+
+export default contactList;
