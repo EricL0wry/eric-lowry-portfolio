e1a3d2649a2bdaa4df4318704c8cdb6d47399491
Eric Lowry <ericmichaellowry@gmail.com>
2020-06-25 17:45:53 -0700

    Moved list variable files to new component data folder

---
+++ src/components/component-data/tools-list.js
@@ -0,0 +1,49 @@
+const toolsList = [
+  {
+    id: 1,
+    name: 'AWS',
+    img: './images/tools/amazonwebservices-original.svg'
+  },
+  {
+    id: 2,
+    name: 'Serverless',
+    img: './images/tools/serverless.svg'
+  },
+  {
+    id: 3,
+    name: 'Git/GitHub',
+    img: './images/tools/github-original.svg'
+  },
+  {
+    id: 4,
+    name: 'Webpack',
+    img: './images/tools/webpack-original.svg'
+  },
+  {
+    id: 5,
+    name: 'npm',
+    img: './images/tools/npm-original-wordmark.svg'
+  },
+  {
+    id: 6,
+    name: 'Babel',
+    img: './images/tools/babel-original.svg'
+  },
+  {
+    id: 7,
+    name: 'VSCode',
+    img: './images/tools/visualstudio-plain.svg'
+  },
+  {
+    id: 8,
+    name: 'Nginx',
+    img: './images/tools/nginx-original.svg'
+  },
+  {
+    id: 9,
+    name: 'Illustrator',
+    img: './images/tools/illustrator-plain.svg'
+  }
+];
+
+export default toolsList;
