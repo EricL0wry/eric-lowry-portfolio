7c7ab6ec0550b5c18b7ccbc2ed2a07cd024101d9
Eric Lowry <ericmichaellowry@gmail.com>
2020-06-27 16:49:00 -0700

    Destructured contact properties

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 
 export default function Contact(props) {
+  const { icon, type, text, label, url } = props.contact;
+  let contactLink;
+
+  if (type === 'single') {
+    contactLink = <h6><a href="https://www.google.com/maps/search/?api=1&Orange+County%2C+CA" target="_blank" rel="noreferrer noopener">Orange County, CA</a></h6>;
+  } else {
+    contactLink = <h1>Multiple</h1>;
+  }
 
   return (
     <div className="col-lg-6 contact-card d-flex justify-content-center align-items-center">
@@ -12,7 +20,7 @@ export default function Contact(props) {
           <h4>Location</h4>
         </div>
         <div className='contact-link-container'>
-          <h6><a href="https://www.google.com/maps/search/?api=1&Orange+County%2C+CA" target="_blank" rel="noreferrer noopener">Orange County, CA</a></h6>
+          {contactLink}
         </div>
       </div>
     </div>
