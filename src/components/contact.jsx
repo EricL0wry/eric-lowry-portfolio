e57696c43457cd0f8b274abf9d25b21aa14adb67
Eric Lowry <ericmichaellowry@gmail.com>
2020-06-27 16:14:34 -0700

    Completed basic structure of a contact item

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -9,12 +9,17 @@ export default function Contact(props) {
           <h1>Get in touch</h1>
         </div>
         <div className="contact-cards row">
-          <div className="col-lg-4 contact-card d-flex flex-column justify-content-center align-items-center">
-            <div className="contact-label-container">
-              <h4>Location</h4>
+          <div className="col-lg-6 contact-card d-flex justify-content-center align-items-center">
+            <div className="contact-icon">
+              <i className="fas fa-map-pin"></i>
             </div>
-            <div className='contact-link-container'>
-              <h6><a href="https://www.google.com/maps/search/?api=1&Orange+County%2C+CA" target="_blank" rel="noreferrer noopener">Orange County, CA</a></h6>
+            <div className="contact-info-container d-flex flex-column">
+              <div className="contact-label-container">
+                <h4>Location</h4>
+              </div>
+              <div className='contact-link-container'>
+                <h6><a href="https://www.google.com/maps/search/?api=1&Orange+County%2C+CA" target="_blank" rel="noreferrer noopener">Orange County, CA</a></h6>
+              </div>
             </div>
           </div>
         </div>
