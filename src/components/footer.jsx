0c617b35aa488474c465f00052b188f02efda8a0
Eric Lowry <ericmichaellowry@gmail.com>
2020-06-29 17:48:47 -0700

    Added footer content and containers

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,6 +3,12 @@ import React from 'react';
 export default function Footer() {
 
   return (
-    <h1>Footer</h1>
+    <footer className="footer grey py-4">
+      <div className="footer-content container">
+        <div className="footer-text row flex-column justify-content-center align-items-center">
+          <h6 className="my-0">&copy; Eric Lowry 2020 | All Rights Reserved</h6>
+        </div>
+      </div>
+    </footer>
   );
 }
