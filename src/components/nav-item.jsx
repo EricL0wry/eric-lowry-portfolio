6e74a39cd676167db7a8e7bff98c32481d26feb0
Eric Lowry <ericmichaellowry@gmail.com>
2020-08-08 10:41:13 -0700

    Enclosed Nav Item in anchor tag

--- src/components/nav-item.jsx
+++ src/components/nav-item.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 
 export default function NavItem(props) {
-  return <div>Nav Item</div>;
+  const { text } = props;
+  return (
+    <li className="nav-item">
+      <a className="nav-link" href="#">
+        {text}
+      </a>
+    </li>
+  );
 }
