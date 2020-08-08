c75d94b3593526fcaf631d27a9922c73e969e300
Eric Lowry <ericmichaellowry@gmail.com>
2020-08-08 09:27:33 -0700

    Added placeholder nav items and fixed positioning

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,5 +1,37 @@
 import React from 'react';
+import NavItem from './nav-item';
 
 export default function Nav() {
-  return <nav className="navbar navbar-expand-xl">This is a Nav</nav>;
+  return (
+    <nav className="navbar fixed-top navbar-light navbar-expand-xl">
+      <div className="container">
+        <span className="navbar-brand">Eric Lowry</span>
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <NavItem />
+            </li>
+            <li className="nav-item">
+              <NavItem />
+            </li>
+            <li className="nav-item">
+              <NavItem />
+            </li>
+            <li className="nav-item">
+              <NavItem />
+            </li>
+            <li className="nav-item">
+              <NavItem />
+            </li>
+            <li className="nav-item">
+              <NavItem />
+            </li>
+            <li className="nav-item">
+              <NavItem />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
