8661ab328e493186848e405f091158d7def20249
Eric Lowry <ericmichaellowry@gmail.com>
2020-08-08 10:47:39 -0700

    Add section ID

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 
 export default function Banner() {
-
   return (
-    <section className="banner">
+    <section className="banner" id="home">
       <div className="banner-content container d-flex flex-column justify-content-center align-items-center">
-        <div className='title-container'>
+        <div className="title-container">
           <h1>Eric Lowry</h1>
         </div>
-        <div className='type-container'>
+        <div className="type-container">
           <h4>I&apos;m a person with fingers</h4>
         </div>
       </div>
