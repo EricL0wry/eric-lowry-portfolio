d6533c234ac710aebd9f985eb5dd6d979fd9ab4b
Eric Lowry <ericmichaellowry@gmail.com>
2020-08-08 09:56:19 -0700

    Create basic nav item list

--- src/components/component-data/nav-item-list.js
+++ src/components/component-data/nav-item-list.js
@@ -1,3 +1,28 @@
-const navItemList = [];
+const navItemList = [
+  {
+    id: 1,
+    text: 'Home'
+  },
+  {
+    id: 2,
+    text: 'About'
+  },
+  {
+    id: 3,
+    text: 'Portfolio'
+  },
+  {
+    id: 4,
+    text: 'Skills'
+  },
+  {
+    id: 5,
+    text: 'Tools'
+  },
+  {
+    id: 6,
+    text: 'Contact'
+  }
+];
 
 export default navItemList;
