a1d9dd4d644aa181a80d05a8be8851763e6a29e8
Eric Lowry <ericmichaellowry@gmail.com>
2020-08-08 10:52:29 -0700

    Add section ID

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -2,21 +2,19 @@ import React from 'react';
 import Skill from './skill';
 
 export default function Skills(props) {
-  const skills = props.skills.map(skillObj => {
+  const skills = props.skills.map((skillObj) => {
     const { id, name, img } = skillObj;
-    return <Skill key={id} name={name} img={img}/>;
+    return <Skill key={id} name={name} img={img} />;
   });
 
   return (
-    <section className="skills">
+    <section className="skills" id="skills">
       <div className="skills-content container sp-top sp-bottom">
         <div className="skills-title row flex-column justify-content-center align-items-center">
           <h4>Skills</h4>
           <h1>What I do</h1>
         </div>
-        <div className="skills-cards row">
-          {skills}
-        </div>
+        <div className="skills-cards row">{skills}</div>
       </div>
     </section>
   );
