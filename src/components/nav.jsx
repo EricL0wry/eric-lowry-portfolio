1910fd15ffb98d42a14ce635490955b8cca621e5
Eric Lowry <ericmichaellowry@gmail.com>
2020-08-08 10:35:04 -0700

    Map through and render nav items

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,35 +1,18 @@
 import React from 'react';
 import NavItem from './nav-item';
 
-export default function Nav() {
+export default function Nav(props) {
+  const navItemElements = props.navItems.map((item) => {
+    const { id, text } = item;
+    return <NavItem key={id} text={text} />;
+  });
+
   return (
     <nav className="navbar fixed-top navbar-light bg-light navbar-expand-xl">
       <div className="container">
         <span className="navbar-brand">Eric Lowry</span>
         <div className="collapse navbar-collapse justify-content-end">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <NavItem />
-            </li>
-            <li className="nav-item">
-              <NavItem />
-            </li>
-            <li className="nav-item">
-              <NavItem />
-            </li>
-            <li className="nav-item">
-              <NavItem />
-            </li>
-            <li className="nav-item">
-              <NavItem />
-            </li>
-            <li className="nav-item">
-              <NavItem />
-            </li>
-            <li className="nav-item">
-              <NavItem />
-            </li>
-          </ul>
+          <ul className="navbar-nav">{navItemElements}</ul>
         </div>
       </div>
     </nav>
